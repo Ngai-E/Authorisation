d792b85d6695fdfc2e37facba9ed6f32e0908503
CHRISTIAN KEVEN <christiankeven326@gmail.com>
2022-08-06 19:37:58 +0100

    Commit

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ docker container run -d  \
 -e MYSQL_USER=watchadd \
 -e CONFIG_SERVER_URL=http://cloud-config-server:56030 \ 
 -e EUREKA_HOST_ZONE=http://cloud-discovery-server:56031/eureka/ \ 
--e MYSQL_PASS=watchadd@2022 auth
+-e MYSQL_PASS=watchadd@2022 ngaie/authentication-service:$2
